130757dc5b7170905235a589e81fab92b59a3ffa
Antonimason <sba23108@student.cct.ie>
2023-11-30 19:21:01 +0000

    First commit

---
+++ src/gittesting/GitTesting.java
@@ -0,0 +1,11 @@
+package gittesting;
+
+public class GitTesting {
+    public static void main(String[] args) {
+       int sum = 9;
+       String hola  = "hola";
+       boolean AEI = true;
+       System.out.println("Be a Hero");
+    }
+    
+}
