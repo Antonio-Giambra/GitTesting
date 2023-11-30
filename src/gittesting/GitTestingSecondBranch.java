d6f0186aabd5700f4afbde018a24bc10d035ad8c
Antonimason <sba23108@student.cct.ie>
2023-11-30 20:27:49 +0000

    test 8

--- src/gittesting/GitTestingSecondBranch.java
+++ src/gittesting/GitTestingSecondBranch.java
@@ -2,9 +2,9 @@
 package gittesting;
 
 public class GitTestingSecondBranch {
+    
     public String hello(String hello){
         String test1 = hello + " User";
         return test1;
     }
-    
 }
