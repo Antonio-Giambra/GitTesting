7897e15077563bba4b5d4b113191d1a3f44fa0a3
Antonimason <sba23108@student.cct.ie>
2023-11-30 20:24:40 +0000

    hola

--- src/gittesting/GitTesting.java
+++ src/gittesting/GitTesting.java
@@ -1,5 +1,13 @@
 package gittesting;
 
 public class GitTesting {
+    public static void main(String[] args) {
+       int sum = 9;
+       String hola = "Hello";
+       boolean AEI = true;
+       GitTestingSecondBranch greet = new GitTestingSecondBranch();
+       
+       System.out.println(greet.hello(hola) + " You most be your own Villian");
+    }
     
 }
